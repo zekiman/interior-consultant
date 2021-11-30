7469a766ef0060a083e15bed376feea6ad66e39a
zekiman <87340558+zekiman@users.noreply.github.com>
2021-11-30 23:00:28 +0200

    updating project

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 // ! DOM ELEMENTS //
 
-let mobileMenuIcon = document.querySelector('.menu-icon-m')
+mobileMenuIcon = document.querySelector('.menu-icon-m')
 
+closeButton = document.querySelector('.close-btn')
 
+container = document.querySelector('#container')
+
+hamburgerMenu = document.querySelector('.hamburger-all-m')
 
 
 
 // ! ADD EVENT LISTENERS
 
-mobileMenuIcon.addEventListener('click',function(e){
-  
+document.addEventListener('click',function(e){
+    // if menu icon is clicked, hide container
+    if(e.target.parentElement==mobileMenuIcon){
+        hideContainer();
+    }
+    // if close button is clicked, hide hamburger
+    if(e.target.parentElement==closeButton){
+        hideHamburger();
+    }
+    
 })
+
+
+// ! FUNCTIONS
+
+// Hide Container Function
+function hideContainer(){
+    container.classList.add('none');
+    hamburgerMenu.classList.remove('none');
+}
+
+// Hide Hamburger Function
+function hideHamburger(){
+    hamburgerMenu.classList.add('none');
+    container.classList.remove('none');
+}
